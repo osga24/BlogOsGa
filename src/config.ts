4c2bf8925abdbe7e1be7bcc5f7e78b43dca2b397
OsGa24 <oscarhuang950324@gmail.com>
2024-08-29 01:38:44 +0800

    Hide the theme color picker for visitors and fix icon

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 160,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
@@ -27,8 +27,8 @@ export const siteConfig: SiteConfig = {
   favicon: [    // Leave this array empty to use the default favicon
     {
       src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-      theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+      // theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      // sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
 }
